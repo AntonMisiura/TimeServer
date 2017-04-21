5bbb4dfd85574f4da9fcbb593f3a04a3f379117f
AntonMisiura <misiura.anton@gmail.com>
2017-04-21 15:47:46 +0300

    Initial commit

---
+++ TimeServer/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TimeServer
+{
+    internal class Program
+    {
+        private static byte [] _buffer = new byte[1024];
+        private static List<Socket> _clientSockets = new List<Socket>();
+        private static Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            SetupServer();
+            Console.ReadLine();
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("setting up server...");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Loopback, 5678));
+            _serverSocket.Listen(10);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void AcceptCallback(IAsyncResult ar)
+        {
+            var socket = _serverSocket.EndAccept(ar);
+            _clientSockets.Add(socket);
+            Console.WriteLine("Client connected");
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+
+            Socket socket = (Socket) ar.AsyncState;
+
+            try
+            {
+                int received = socket.EndReceive(ar);
+                byte[] dataBuf = new byte[received];
+                Array.Copy(_buffer, dataBuf, received);
+
+                string text = Encoding.ASCII.GetString(dataBuf);
+                Console.WriteLine("Text received: " + text);
+
+                if (text.ToLower() == "get time")
+                {
+                    SendData(DateTime.Now.ToString(), socket);
+                }
+                else
+                {
+                    SendData("invalid request", socket);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("client disconnected");
+                socket.Close();
+                socket.Dispose();
+            }
+        }
+
+        private static void SendData(string toString, Socket socket)
+        {
+            var data = Encoding.ASCII.GetBytes(toString);
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            var socket = (Socket)ar.AsyncState;
+            socket.EndSend(ar);
+        }
+    }
+}
